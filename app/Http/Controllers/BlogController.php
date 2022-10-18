7b2669c61c33e17c99450b1b9e5ed7a355b8eab1
jim-nnamdi <jimsamuel50@gmail.com>
2022-10-18 20:12:26 +0100

    setting up auth and blog logic

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+
+    public function index(){
+        $articles = Blog::latest()->paginate(9);
+        return view("blog.index", [
+            "articles" => $articles,
+        ]);
+    }
+
+    public function create(){
+        return view("blog.create");
+    }
+
+    public function store(Blog $blog, Request $request) {
+        $blog = new Blog();
+        $blog->title = $request->title;
+        $blog->description = $request->description;
+        $blog->author = $request->author;
+        $blog->image = $request->image;
+        $blog->save();
+        redirect("/blog");
+    }
 }
