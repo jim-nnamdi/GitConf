42a56c72bf7075284a1b67162d1166f0a75ba96d
jim-nnamdi <jimsamuel50@gmail.com>
2022-11-01 08:03:46 +0100

    data modelling & data retrieval for payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function __construct(){
+        return $this->middleware("auth");
+    }
+
+    public function index(){
+        $allPayments = Payment::latest()
+        ->where("approved", true)
+        ->where("debited", true)
+        ->paginate(12);
+
+        return view("payments.index", [
+            "allPayments" => $allPayments,
+        ]);;
+    }
+
+    public function getPaymentByDepartment(Request $request){
+        $paymentByDepartment = Payment::where("department", $request->department)
+        ->where("approved", true)
+        ->paginate(12);
+        return view("payment.department", [
+            "paymentByDepartment" => $paymentByDepartment,
+        ]);
+    }
 }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Blog extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name","description","author", "image", "approved"];
 }
